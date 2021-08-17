e28946e56ab870d86707e578c13a8a78f45f24d2
商华伟 <xihaouwei@aliyun.com>
2021-08-17 21:14:13 +0800

    task: 实现生成模板

--- bin/vue.js
+++ bin/vue.js
@@ -11,7 +11,20 @@ program
     .usage('<command> [options]')
 
 
-// create 命令 3.x创建项目命令
+/**
+ * create 命令 3.x创建项目命令
+ * -p, --preset <presetName>       忽略提示符并使用已保存的或远程的预设选项
+ * -d, --default                   忽略提示符并使用默认预设选项
+ * -i, --inlinePreset <json>       忽略提示符并使用内联的 JSON 字符串预设选项
+ * -m, --packageManager <command>  在安装依赖时使用指定的 npm 客户端
+ * -r, --registry <url>            在安装依赖时使用指定的 npm registry
+ * -g, --git [message]             强制 / 跳过 git 初始化，并可选的指定初始化提交信息
+ * -n, --no-git                    跳过 git 初始化
+ * -f, --force                     覆写目标目录可能存在的配置
+ * -c, --clone                     使用 git clone 获取远程预设选项
+ * -x, --proxy                     使用指定的代理创建项目
+ * -b, --bare                      创建项目时省略默认组件中的新手指导信息
+ */
 program
     .command('create <app-name>')
     .description('create a new project powered by vue-cli-service')
@@ -31,7 +44,7 @@ program
     .action((name, otherD, cmd) => {
         require('../lib/src3.0/create')(name, otherD)
     })
-
+// 输出使用帮助信息
 program.on('--help', () => {
     console.log()
     console.log(`  Run ${chalk.cyan(`vue <command> --help`)} for detailed usage of given command.`)
